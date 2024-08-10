4eb56642ba63288fa9da36f88bb78dd3cf4462a3
Soumya Ranjan Pratap <srpratap021@gmail.com>
2024-08-10 20:15:07 +0530

    Student Management System using Entity Framework

---
+++ Models/Studentinitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace StudentManagementSystem.Models
+{
+    public class Studentinitializer : DropCreateDatabaseIfModelChanges<StudentContext>
+    {
+        protected override void Seed(StudentContext context)
+        {
+            var Students = new List<Student>
+            {
+                new Student { Id = 1, Name = "Soumya", Date = DateTime.Now },
+                new Student { Id = 2, Name = "Satya", Date = DateTime.Parse("2024-12-23") }
+            };
+
+            Students.ForEach(s => context.Students.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
